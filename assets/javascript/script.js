ae8deb95cdbf8c0b26591f03d7b32f53b287551c
Eric Crain <ericcrain77@gmail.com>
2022-09-20 16:45:49 -0400

    added javascript file and functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+function calculate() {
+    var p = document.getElementById("result").value;
+
+    var q = eval(p);
+
+    document.getElementById("result").value = q;
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function clear() {
+    document.getElementById("result").value = "";
+}
