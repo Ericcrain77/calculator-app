87d8f0622265684f5c2cd22c98be7c3fef12c9b4
Eric Crain <ericcrain77@gmail.com>
2022-09-28 16:00:46 -0400

    removing the GBA stuff

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,19 +13,3 @@ function display(value) {
 function clearNumbers() {
     document.getElementById("result").value = " ";
 }
-
-function display2(value) {
-    document.getElementById("result2").value += value;
-}
-
-function calculate2() {
-    var p = document.getElementById("result2").value;
-
-    var q = eval(p);
-
-    document.getElementById("result2").value = q;
-}
-
-function clearNumbers2() {
-    document.getElementById("result2").value = " ";
-}
